22d7cbbb2368f073ea5f12a4e82893101f09f7c8
Kibuye24 <93917125+Kibuye24@users.noreply.github.com>
2022-11-17 04:38:41 +0300

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,7 @@
-import Head from 'next/head';
-import {PostCard, PostWidgets, Categories} from '../components';
-import { getPosts } from '../services';
-import { FeaturedPosts } from '../sections/index';
-
+import Head from "next/head";
+import { PostCard, PostWidgets, Categories } from "../components";
+import { getPosts } from "../services";
+import { FeaturedPosts } from "../sections/index";
 
 export default function Home({ posts }) {
   return (
@@ -12,13 +11,14 @@ export default function Home({ posts }) {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <FeaturedPosts />
-      <div className='grid grid-cols-1 lg:grid-cols-12 gap-12'>
-        <div className='lg:col-span-8 col-span-1'>
+      <div className="grid grid-cols-1 lg:grid-cols-12 gap-12">
+        <div className="lg:col-span-8 col-span-1">
           {posts.map((post, index) => (
-            <PostCard post={post.node} key={index}/>))}
+            <PostCard key={index} post={post.node} />
+          ))}
         </div>
-        <div className='lg:col-span-4 col-span-1'>
-          <div className='sticky relative top-8'>
+        <div className="lg:col-span-4 col-span-1">
+          <div className="sticky relative top-8">
             <PostWidgets />
             <Categories />
           </div>
@@ -35,5 +35,3 @@ export async function getStaticProps() {
     props: { posts },
   };
 }
-
-
